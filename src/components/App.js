741bb8d2616bfffc05724b74c828390987778735
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-12 09:09:00 +0100

    Import PokeList to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "../styleSheets/App.css";
 import React from "react";
 import pokemon from "../data/pokemon.json";
+import PokeList from "./PokeList";
 
 class App extends React.Component {
   render() {
