32c2a49c5771097e36a560fbe52af492fd3be7fd
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-12 09:02:49 +0100

    Import pokemon.json to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import "../styleSheets/App.css";
 import React from "react";
+import pokemon from "../data/pokemon.json";
 
 class App extends React.Component {
   render() {
-    return <div className="container"></div>;
+    return <div className="container">{pokemon}</div>;
   }
 }
 
