3cbba71a93301a12380c391209028573121e20a9
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-12 09:20:48 +0100

    Create types list form this.props.types with map method

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 
 class Pokemon extends React.Component {
   render() {
-    console.log(this.props.url);
-    console.log(this.props.name);
-    console.log(this.props.types);
+    const typesList = this.props.types.map((item, index) => (
+      <li key={index} className="list">
+        {item}
+      </li>
+    ));
     return (
       <div className="poke-card">
         <img src={this.props.url} alt={this.props.name} className="poke-image" />
         <div className="text-container">
           <h2 className="poke-name">{this.props.name}</h2>
-          <ul className="type-list"></ul>
+          <ul className="type-list">{typesList}</ul>
         </div>
       </div>
     );
