e45a7624a71beffbeacf2f4daa79a04bb8fe31cd
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-09 09:45:54 +0100

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,13 @@ import "../styleSheets/App.scss";
 import React from "react";
 import pokemon from "../data/pokemon.json";
 import PokeList from "./PokeList";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <header>
-          <h1 className="title">¡Hazte con todos!</h1>
-        </header>
+        <Header />
         <main>
           <PokeList />
         </main>
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="title">¡Hazte con todos!</h1>
+    </header>
+  );
+};
+export default Header;
