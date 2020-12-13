397e2cb6de5934c4cde543132bb6b11498ae4f36
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-13 21:09:49 +0100

    Create PokeDetail component

---
+++ src/components/PokeDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+class PokeDetail extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    return()
+  }
+}
+PokeDetail.propTypes = {
+}
+export default PokeDetail
