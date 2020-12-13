4840a55d0d14e76ded324bd2d72e0d0732e86340
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-13 21:12:31 +0100

    Install react router

--- src/components/PokeDetail.js
+++ src/components/PokeDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-class PokeDetail extends React.Component {
+class PokeDetail extends Component {
   constructor(props) {
     super(props)
   }
